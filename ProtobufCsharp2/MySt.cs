cce4262aa0c3203af3c5b8bdd76acf586ed6f7ff
Kei Tashima <tassi.yuzukko@gmail.com>
2018-04-03 23:14:12 +0900

    protobufのやり方に、JSON経由した

---
+++ ProtobufCsharp2/MySt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProtobufCsharp2
+{
+    public partial class MySt
+    {
+        public int A { get; set; }
+        public string B { get; set; }
+        public MyT Child { get; set; }
+        public int C { get; set; }
+        public bool D { get; set; }
+        public Dictionary<string, int> Dict { get; set; }
+        public byte[] ArrayOfBytes { get; set; }
+    }
+
+    public partial class MyT
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+    }
+}
--- ProtobufCsharp2/Program.cs
+++ ProtobufCsharp2/Program.cs
@@ -3,6 +3,9 @@ using System;
 using System.IO;
 using System.Text;
 using test;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
 
 namespace ProtobufCsharp2
 {
@@ -12,8 +15,56 @@ namespace ProtobufCsharp2
 
         static void Main(string[] args)
         {
+            var bytes = SerializeObject();
+            DeserializeObject(bytes);
+
+            Console.ReadLine();
+        }
+
+        static byte[] SerializeObject()
+        {
+            var dict = new Dictionary<string, int>();
+            dict.Add("tashima", 32);
+            dict.Add("Sato", 45);
+            dict.Add("Ueda", 52);
+            dict.Add("Venus", 23);
+            dict.Add("Watson", 3);
+
+            var mySt = new MySt
+            {
+                A = 123456789,
+                B = "tashimakeidesu",
+                Child = new MyT
+                {
+                    X = 9999,
+                    Y = 8888,
+                    Z = 7777,
+                },
+                C = -123456789,
+                D = true,
+                Dict = dict,
+                ArrayOfBytes = new byte[] { 1, 2, 3, 5, 8, 13, 21, 34, 55 },
+            };
+
+            var setting = new JsonSerializerSettings { MissingMemberHandling = MissingMemberHandling.Ignore, NullValueHandling = NullValueHandling.Ignore, Formatting = Formatting.Indented, ContractResolver = new CamelCasePropertyNamesContractResolver() };
+            var json = JsonConvert.SerializeObject(mySt, setting);
+
+            JsonParser jsonParser = new JsonParser(JsonParser.Settings.Default);
+
+            var st = jsonParser.Parse<st>(json);
+            return Serialize(st);
+        }
+
+        static void DeserializeObject(byte[] bytes)
+        {
             // デシリアライズ
-            st st = Deserialize<st>(ReadFile(FileName));
+            st st = Deserialize<st>(bytes);
+
+            JsonFormatter jsonFormatter = new JsonFormatter(JsonFormatter.Settings.Default);
+            var json = jsonFormatter.Format(st);
+
+            var setting = new JsonSerializerSettings { MissingMemberHandling = MissingMemberHandling.Ignore, NullValueHandling = NullValueHandling.Ignore, Formatting = Formatting.Indented, ContractResolver = new CamelCasePropertyNamesContractResolver() };
+            var mySt = JsonConvert.DeserializeObject<MySt>(json, setting);
 
             Console.WriteLine("Read Success !!");
             Console.WriteLine($"A:{st.A}, B:{st.B}, C:{st.C}, D:{st.D}, Child.X:{st.Child.X}, Child.Y:{st.Child.Y}");
@@ -21,7 +72,6 @@ namespace ProtobufCsharp2
             {
                 Console.WriteLine($"key:{item.Key}, value:{item.Value}");
             }
-            Console.ReadLine();
         }
 
         static byte[] ReadFile(string filename)
@@ -36,6 +86,15 @@ namespace ProtobufCsharp2
                 fs.Read(bs, 0, bs.Length);
 
                 return bs;
+            }
+        }
+
+        static byte[] Serialize<T>(T obj) where T : IMessage<T>
+        {
+            using (var stream = new MemoryStream())
+            {
+                obj.WriteTo(stream);
+                return stream.ToArray();
             }
         }
 
